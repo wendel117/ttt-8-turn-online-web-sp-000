13c5eac1b96fccf0bda3b6deaf7779839eb063d0
Wendy Hamilton <wdhamilton117@gmail.com>
2019-01-24 19:04:48 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -41,14 +41,4 @@ def turn(board)
     turn(board)
   end
 end
-def turn(board)
-  puts "Please enter 1-9:"
-  turn = gets.strip
-  index = input_to_index(turn)
-  if valid_move?(board, index)
-    move(board, index)
-    display_board(board)
-  else
-    turn(board)
-  end
-end
+
