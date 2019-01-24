f81b58907b887aae4397941110792bf9e5aa2a4a
Wendy Hamilton <wdhamilton117@gmail.com>
2019-01-24 18:55:20 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,36 @@
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(array, index, value = "X")
+  array[index]=value
+end
+
+def valid_move?(board, index)
+  if position_taken?(board, index) == false && index.between?(0, 8)
+    return true
+  else
+    return false
+  end
+end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == nil || board[index] == ""
+    return false
+  else
+    return true
+  end
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input=gets.strip
+end
